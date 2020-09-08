852fe450c4ef61302a0b239d1815f551d01fd00f
jiashiyang <jia_shiyang@sina.com>
2020-09-08 14:25:08 +0800

    Excel 子服务 ExcelToJsonFile 事例完善。
    按照Excel解析规则生成json文件，放置到命名规范决定的文件夹结构中。

--- Excel/app/services/Excel/ExcelToJsonFile/ExcelToJsonFile.py
+++ Excel/app/services/Excel/ExcelToJsonFile/ExcelToJsonFile.py
@@ -2,6 +2,9 @@
 # Created by jiasy at 2020/9/7
 from base.supports.Base.BaseInService import BaseInService
 from utils import *
+import os
+from utils.excelUtil import WorkBook
+from utils.excelUtil.Sheet import SheetType
 
 
 class ExcelToJsonFile(BaseInService):
@@ -16,7 +19,23 @@ class ExcelToJsonFile(BaseInService):
         super(ExcelToJsonFile, self).destory()
 
     def doExcelFunc(self, dParameters_: dict):
-        dictUtils.showDictStructure(dParameters_)
+        # 确保路径正确
+        _sourceFolderPath = sysUtils.folderPathFixEnd(dParameters_["sSourceFolder"])
+        _targetFolderPath = sysUtils.folderPathFixEnd(dParameters_["sTargetFolder"])
+        _xlsxFilePathList = folderUtils.getFileListInFolder(_sourceFolderPath, [".xlsx"])
+        for _idx in range(len(_xlsxFilePathList)):
+            _xlsxFilePath = _xlsxFilePathList[_idx]
+            # # 变更执行权限
+            # sysUtils.chmod("666", _xlsxFilePath)
+            # xlsx文件 -> 文件夹，包含了将要生成json文件。
+            _xlsxFolderPath = fileUtils.getNewNameKeepFolderStructure(
+                _sourceFolderPath, _targetFolderPath, _xlsxFilePath
+            )
+            _currentWorkBook = WorkBook.WorkBook()  # 解析WorkBook
+            _currentWorkBook.initWithWorkBook(_xlsxFilePath)
+            _currentWorkBook.toJsonFile(_xlsxFolderPath)  # 内容解析成json，写入给定文件夹
+        print("Excel -> Json : ")
+        folderUtils.showDirFile(_targetFolderPath, 0, " " * 4)
 
 
 import Main
@@ -28,10 +47,10 @@ if __name__ == "__main__":
         _baseServiceName,
         _subBaseInServiceName,
         {  # 所需参数
-            "sourceFolder": "{sResPath}/source",
-            "targetFolder": "{sResPath}/target"
+            "sSourceFolder": "{sResPath}/source",
+            "sTargetFolder": "{sResPath}/target"
         },
         {  # 命令行进行的全局参数修改
-            "publicType": "test"
+            "sPublicType": "test"
         }
     )
